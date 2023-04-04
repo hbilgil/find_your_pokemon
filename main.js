503aa37a2bb849700c5e59ecf0f24bf24183f8a4
Halil Bilgil <hbilgil@gmail.com>
2023-04-04 13:19:47 +0300

    Edit main.js to declare a function to fetch pokemons by API

--- main.js
+++ main.js
@@ -42,6 +42,15 @@ const matchPokemonSearch = () => {
     })
 };
 
+//A function to fetch pokemon for each integer value
+
+const fetchPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    createPokemonCard(data);
+}
+
 //A function to get ALL pokemons  defined in API
 
 const getPokemon = async () => {
